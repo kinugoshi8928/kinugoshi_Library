9ef6f4e107efc09bc9e617c96dc700f0bbe2d477
kinugoshi8928 <kappagloop@gmail.com>
2023-01-25 22:06:42 +0900

    Unionfind_map

---
+++ kinugoshi/Unionfind_map.hpp
@@ -0,0 +1,35 @@
+#ifndef kinugoshi_UF_MAP
+#define kinugoshi_UF_MAP
+#include <map>
+
+namespace kinugoshi {
+
+template <class UF_MAP>
+struct Unionfind_map {
+    // map版UF
+    // 初期化 Unionfind_map<string> tree; など
+    Unionfind_map<UF_MAP>() {}
+    UF_MAP leader(UF_MAP x) {
+        if(par[x] == x)return x;
+        else return par[x] = leader(par[x]);
+    }
+    void merge(UF_MAP x,UF_MAP y){
+        if(par.find(x)==par.end()) par[x] = x;
+        if(par.find(y)==par.end()) par[y] = y;
+        UF_MAP lx=leader(x),ly=leader(y);
+        if(lx==ly)return;
+        if(rank[lx]<rank[ly]) par[lx] = ly;
+        else{
+            par[ly] = lx;
+            if(rank[lx]==rank[ly]) rank[lx] ++;
+        }
+    }
+    bool same(UF_MAP x,UF_MAP y){
+        return root(x) == root(y);
+    }
+
+   private:
+    std::map<UF_MAP, int> par,rank;
+};
+}  // namespace kinugoshi
+#endif  // kinugoshi_UF_MAP
