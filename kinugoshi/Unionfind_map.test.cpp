c7c9a9c1006eeefff54ac6a29b27cb4879036864
kinugoshi8928 <kappagloop@gmail.com>
2023-01-25 23:41:23 +0900

    なんかライブラリバグらせてたバカがいるらしい

---
+++ kinugoshi/Unionfind_map.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+#include "kinugoshi/Unionfind_map.hpp"
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using namespace kinugoshi;
+using ll = long long;
+using mint = modint998244353;
+#define all(v) v.begin(), v.end()
+#define rep(a, b) for (long long a = 0; a < (long long)(b); a++)
+int op(int a, int b) { return min(a, b); }
+int e() { return 2147483647; }
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+    Unionfind_map<int> UF;
+    rep(i, Q) {
+        int t, u, v;
+        cin >> t >> u >> v;
+        if (t == 0) UF.merge(u, v);
+        else cout << UF.same(u, v) << endl;
+    }
+}
--- kinugoshi/temp.cpp
+++ kinugoshi/temp.cpp
@@ -1,4 +1,6 @@
-#pragma once
+#line 1 "Unionfind_map.test.cpp"
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+#line 2 "Unionfind_map.hpp"
 #ifndef kinugoshi_UF_MAP
 #define kinugoshi_UF_MAP
 #include <map>
@@ -35,3 +37,27 @@ struct Unionfind_map {
 };
 }  // namespace kinugoshi
 #endif  // kinugoshi_UF_MAP
+#line 3 "Unionfind_map.test.cpp"
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using namespace kinugoshi;
+using ll = long long;
+using mint = modint998244353;
+#define all(v) v.begin(), v.end()
+#define rep(a, b) for (long long a = 0; a < (long long)(b); a++)
+int op(int a, int b) { return min(a, b); }
+int e() { return 2147483647; }
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+    Unionfind_map<int> UF;
+    rep(i, Q) {
+        int t, u, v;
+        cin >> t >> u >> v;
+        if (t == 0) UF.merge(u, v);
+        else cout << UF.same(u, v) << endl;
+    }
+}
