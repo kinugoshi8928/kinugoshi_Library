7e18eb2c1a594f6a39a08b8a2daa2d70fd69450c
kinugoshi8928 <kappagloop@gmail.com>
2023-01-26 18:20:17 +0900

    ACLの残骸を消したはず

--- kinugoshi/Unionfind_map.test.cpp
+++ kinugoshi/Unionfind_map.test.cpp
@@ -4,11 +4,8 @@
 using namespace std;
 using namespace kinugoshi;
 using ll = long long;
-using mint = modint998244353;
 #define all(v) v.begin(), v.end()
 #define rep(a, b) for (long long a = 0; a < (long long)(b); a++)
-int op(int a, int b) { return min(a, b); }
-int e() { return 2147483647; }
 
 int main() {
     int N, Q;
