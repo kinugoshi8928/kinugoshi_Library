27bda6dfbc5bc8c80b13b92355e9b16fdb665206
kinugoshi8928 <kappagloop@gmail.com>
2023-01-29 20:51:55 +0900

    docs

--- kinugoshi/Unionfind_map.hpp
+++ kinugoshi/Unionfind_map.hpp
@@ -2,15 +2,35 @@
 #ifndef kinugoshi_UF_MAP
 #define kinugoshi_UF_MAP
 #include <map>
+/**
+ * @file Unionfind_map.hpp
+ * @brief unionfindの頂点をmapで管理できるようにしたやつ
+ * $log$がついた代わりに頂点番号の上限がなくなった
+ * @note 使うかこれ?
+ */
 
 namespace kinugoshi {
 
 template <class UF_MAP>
+/**
+ * @brief Unionfindの頂点番号上限ない版
+ * 頂点追加はmerge(追加したい頂点,追加したい頂点)
+ * 初期化:Unionfind_map<頂点番号> UF
+ */
 struct Unionfind_map {
+    /**
+     * @brief 親を求める
+     * @param x 頂点番号
+     * @return UF_MAP xの親
+     */
     UF_MAP leader(UF_MAP x) {
         if (par[x] == x) return x;
         else return par[x] = leader(par[x]);
     }
+    /**
+     * @brief 辺を2つの間につける
+     * 頂点の追加もこれで
+     */
     void merge(UF_MAP x, UF_MAP y) {
         if (par.find(x) == par.end()) par[x] = x;
         if (par.find(y) == par.end()) par[y] = y;
@@ -22,6 +42,11 @@ struct Unionfind_map {
             if (rank[lx] == rank[ly]) rank[lx]++;
         }
     }
+    /**
+     * @brief 2つがつながっているかどうか
+     * @return true つながっている
+     * @return false つながっていない
+     */
     bool same(UF_MAP x, UF_MAP y) {
         if (par.find(x) == par.end()) par[x] = x;
         if (par.find(y) == par.end()) par[y] = y;
--- kinugoshi/overwrite_stl.hpp
+++ kinugoshi/overwrite_stl.hpp
@@ -1,19 +1,51 @@
 #include <iostream>
 #include <utility>
 #include <vector>
+/**
+ * @file overwrite_stl.hpp
+ * @brief STLのオーバーライド寄せ集め
+ * 今のところcin/coutのオーバーライドのみ
+ * @details cin/coutをpair/vectorに対応させる
+ * @note 早解き用なので、デバッグ用ではない
+ * デバッグにはgdb+VSCodeを使おう
+ */
 namespace std {
-// pair
+/**
+ * @brief pair型のostreamオーバーライド
+ * @tparam T v.firstの型
+ * @tparam U v.secondの型
+ * @param[in] os ostreamをもってきただけ
+ * @param[in] v 出力するpair
+ * {1,4}ならば"1 4￥n"となる
+ * @return ostream& お作法
+ */
 template <typename T, typename U>
 ostream &operator<<(ostream &os, const pair<T, U> &v) {
     os << v.first << " " << v.second;
     return os;
 }
+/**
+ * @brief pair型のistreamオーバーライド
+ * @tparam T v.firstの型
+ * @tparam U v.secondの型
+ * @param[in] is istreamをもってきただけ
+ * @param[out] v 入力されるpair型
+ * "1 4/n"と入力されているならば {1,4}となる
+ * @return istream& お作法
+ */
 template <typename T, typename U>
 istream &operator>>(istream &is, pair<T, U> &v) {
     is >> v.first >> v.second;
     return is;
 }
-// vector
+/**
+ * @brief vector型のostreamオーバーライド
+ * @tparam T vectorの型
+ * @param[in] os ostreamをもってきただけ
+ * @param v 出力するvector
+ * {1,4,4,5}ならば"1 4 4 5￥n"となる
+ * @return ostream& お作法
+ */
 template <typename T>
 ostream &operator<<(ostream &os, const vector<T> &v) {
     for (int i = 0; i < (int)v.size(); i++) {
@@ -21,6 +53,14 @@ ostream &operator<<(ostream &os, const vector<T> &v) {
     }
     return os;
 }
+/**
+ * @brief vector型のistreamオーバーライド
+ * @tparam T vectorの型
+ * @param[in] is istreamをもってきただけ
+ * @param[out] v 入力されるvector
+ * 要素数だけ入力されるので初期化を忘れないように
+ * @return istream& お作法
+ */
 template <typename T>
 istream &operator>>(istream &is, vector<T> &v) {
     for (T &in : v) is >> in;
